13ff8f511e895d2d33caa9e4ff709d551dd8cdf3
Neeladry <150279194+Neeladry@users.noreply.github.com>
2024-07-28 00:21:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Screen1 from './screens/Screen1';
+
+function App() {
+  return (
+    <div>
+      <Screen1 />
+      
+    </div>
+  );
+}
+
+export default App;
